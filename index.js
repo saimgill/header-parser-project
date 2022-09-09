0ccd9a0e80c731a2518fdef9e6d7cdb04ff922ad
Saim Gill <73778019+saimgill@users.noreply.github.com>
2022-09-09 19:22:30 +0000

    done

--- index.js
+++ index.js
@@ -20,8 +20,9 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', function (req, res) {
+  const headers = req.headers;
+  res.json({ipaddress: headers['x-forwarded-for'], language: headers['accept-language'], software: req.headers['user-agent']})
 });
 
 // listen for requests :)
